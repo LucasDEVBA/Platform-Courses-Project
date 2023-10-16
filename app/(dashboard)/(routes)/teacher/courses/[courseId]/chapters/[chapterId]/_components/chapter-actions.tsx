b9f0997bc059feba2a1ca7daaf00577dce67b7b4
Lucas <lucasdevba@gmail.com>
2023-10-16 14:13:54 -0300

    "feat: add functionallity of  chapter-actions"

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Trash } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import ConfirmModal from "@/components/modals/confirm-modal";
+
+interface ChapterActionsProps {
+  disabled: boolean;
+  courseId: string;
+  chapterId: string;
+  isPublished: boolean;
+}
+
+const ChapterActions = ({
+  disabled,
+  courseId,
+  chapterId,
+  isPublished,
+}: ChapterActionsProps) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+      toast.success("Capítulo deletado");
+      router.refresh();
+      router.push(`/teacher/courses/${courseId}`);
+    } catch (error) {
+      toast.error("Ocorreu um erro!");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button
+        onClick={() => {}}
+        disabled={disabled || isLoading}
+        variant={"outline"}
+        size="sm"
+      >
+        {isPublished ? "Despublicar" : "Publicar"}
+      </Button>
+      <ConfirmModal
+        onConfirm={onDelete}
+        title="Tem certeza?"
+        description="Essa ação não pode ser desfeita"
+      >
+        <Button size={"sm"} disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
+
+export default ChapterActions;
