08c2654deb80ddbb50be0d5505aaeefef2a4c78d
Lucas <lucasdevba@gmail.com>
2023-10-16 02:26:03 -0300

    "feat: add chapter descrition form to project"

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -6,6 +6,7 @@ import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
 import ChapterTitleForm from "./_components/chapter-title-form";
+import ChapterDescriptionForm from "./_components/chapter-description-form";
 
 const ChapterIdPage = async ({
   params,
@@ -67,6 +68,11 @@ const ChapterIdPage = async ({
               <h2 className="text-xl">Personalize seu capítulo</h2>
             </div>
             <ChapterTitleForm
+              initialData={chapter}
+              chapterId={params.chapterId}
+              courseId={params.courseId}
+            />
+            <ChapterDescriptionForm
               initialData={chapter}
               chapterId={params.chapterId}
               courseId={params.courseId}
