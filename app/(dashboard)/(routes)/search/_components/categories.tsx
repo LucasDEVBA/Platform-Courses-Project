146128e231be6ded358da992a3dd488132d8a729
Lucas <lucasdevba@gmail.com>
2023-10-17 23:39:41 -0300

    "feat: atualization name categories"

--- app/(dashboard)/(routes)/search/_components/categories.tsx
+++ app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -19,13 +19,13 @@ interface CategoriesProps {
 }
 
 const iconMap: Record<Category["name"], IconType> = {
-  Music: FcMusic,
-  Photography: FcOldTimeCamera,
+  Musica: FcMusic,
+  Fotografia: FcOldTimeCamera,
   Fitness: FcSportsMode,
-  Accounting: FcSalesPerformance,
-  "Computer Science": FcMultipleDevices,
-  Filming: FcFilmReel,
-  Engineering: FcEngineering,
+  Contabilidade: FcSalesPerformance,
+  "Ciencia da Computacao": FcMultipleDevices,
+  Filmagen: FcFilmReel,
+  Engenharia: FcEngineering,
 };
 
 const Categories = ({ items }: CategoriesProps) => {
